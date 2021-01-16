56b5ee7c2029849bb595e917801eb71249b26126
andrejsmikis <70051330+andrejsmikis@users.noreply.github.com>
2021-01-16 19:18:12 +0200

    Update klasesTehnikaf.js

--- js/klasesTehnikaf.js
+++ js/klasesTehnikaf.js
@@ -5,9 +5,9 @@ async function klasesTehnikaf() {
     let telpa, tips;
     let irProjektors, irSkandas,vajagSkandas, vajagDatoru,vajagProjektoru;
     //let datoruDB =fetch('https://armandspucs.github.io/majas-darbs-1/data/datoruDB.json')
-    let roomDB = await fetch('https://andrejstehnika.amikis.repl.co/dati/telpas')
+    let roomDB = await fetch('https://andrejstehnika.amikis.repl.co/api/dati/telpas')
     let roomJson = await roomDB.json();
-    let datoruDB = await fetch('https://andrejstehnika.amikis.repl.co/dati/tehnika')
+    let datoruDB = await fetch('https://andrejstehnika.amikis.repl.co/api/dati/tehnika')
     let pcJson = await datoruDB.json();
     let kabinetuSkaits=roomJson.dati.length;   
     let garums=pcJson.dati.length;
