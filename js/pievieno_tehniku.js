57e92625613954bade8bea24a8cd642584a68c97
andrejsmikis <70051330+andrejsmikis@users.noreply.github.com>
2021-02-07 00:07:01 +0200

    Update pievieno_tehniku.js

--- js/pievieno_tehniku.js
+++ js/pievieno_tehniku.js
@@ -1,9 +1,9 @@
 async function pievieno_tehniku()
 {
 
-let requestBodyJson = {
+//let requestBodyJson = {
              // "skaits": parseInt(document.querySelector('#skaits').value),            
-             "tips": document.querySelector('#tehnika').value 
+             //"tips": document.querySelector('#tehnika').value 
              //"inventaraNr": document.querySelector("input[name='inventaraNr']").value,
              //"nosaukums": document.querySelector("input[name='nosaukums']").value,
              //"iegadesGads": document.querySelector("input[name='iegadesGads']").value,
@@ -15,23 +15,25 @@ let requestBodyJson = {
              //"ram": document.querySelector("input[name='ram']").value,
              ///"cietaisDisks": document.querySelector("input[name='cietaisDisks']").value,
              //"programmatura": document.querySelector("input[name='programmatura']").value
-              };
-let requestBodyString = JSON.stringify(requestBodyJson);
+//              };
+//let requestBodyString = JSON.stringify(requestBodyJson);
 
 
-let request = await fetch('https://andrejstehnika.amikis.repl.co/static/datorudb.json',
-         		{
-            method:"POST",
-  					headers:
-  					{
-            'X-API-KEY': 'abcd',
-            'Content-Type': 'application/json'
-            },
-            body:requestBodyString
-        		});
+//let request = await fetch('https://andrejstehnika.amikis.repl.co/static/datorudb.json',
+//         		{
+//            method:"POST",
+//  					headers:
+//  					{
+//            'X-API-KEY': 'abcd',
+//            'Content-Type': 'application/json'
+//            },
+//            body:requestBodyString
+//        		});
 
-let atbilde = await request.json();
-console.log(atbilde);
+//let atbilde = await request.json();
+let datoruDB = await fetch('https://andrejstehnika.amikis.repl.co/static/datorudb.json')
+let pcJson = await datoruDB.json();
+console.log(pcJson);
 
 
 }
