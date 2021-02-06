b65514b12371d60ca082b7ba5ace6836ef5e8fd0
andrejsmikis <70051330+andrejsmikis@users.noreply.github.com>
2021-02-06 23:51:34 +0200

    Update pievieno_tehniku.js

--- js/pievieno_tehniku.js
+++ js/pievieno_tehniku.js
@@ -3,18 +3,18 @@ async function pievieno_tehniku()
 
 let requestBodyJson = {
              // "skaits": parseInt(document.querySelector('#skaits').value),            
-             //"tips": document.querySelector('#tehnika').value,
-             "inventaraNr": document.querySelector("input[name='inventaraNr']").value,
-             "nosaukums": document.querySelector("input[name='nosaukums']").value,
-             "iegadesGads": document.querySelector("input[name='iegadesGads']").value,
-             "piegadatajs": document.querySelector("input[name='piegadatajs']").value,
-             "razotajs" : document.querySelector("input[name='razotajs']").value,
-             "kabinetaNr" : document.querySelector("input[name='kabinetaNr']").value,
-             "atbildigais" : document.querySelector("input[name='atbildigais']").value,
-             "procesors": document.querySelector("input[name='procesors']").value,
-             "ram": document.querySelector("input[name='ram']").value,
-             "cietaisDisks": document.querySelector("input[name='cietaisDisks']").value,
-             "programmatura": document.querySelector("input[name='programmatura']").value
+             "tips": document.querySelector('#tehnika').value 
+             //"inventaraNr": document.querySelector("input[name='inventaraNr']").value,
+             //"nosaukums": document.querySelector("input[name='nosaukums']").value,
+             //"iegadesGads": document.querySelector("input[name='iegadesGads']").value,
+             //"piegadatajs": document.querySelector("input[name='piegadatajs']").value,
+             //"razotajs" : document.querySelector("input[name='razotajs']").value,
+             //"kabinetaNr" : document.querySelector("input[name='kabinetaNr']").value,
+             //"atbildigais" : document.querySelector("input[name='atbildigais']").value,
+             //"procesors": document.querySelector("input[name='procesors']").value,
+             //"ram": document.querySelector("input[name='ram']").value,
+             ///"cietaisDisks": document.querySelector("input[name='cietaisDisks']").value,
+             //"programmatura": document.querySelector("input[name='programmatura']").value
               };
 let requestBodyString = JSON.stringify(requestBodyJson);
 
