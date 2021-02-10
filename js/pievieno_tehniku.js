ec706a04c3997a8ad5d63bc9351fd07333b4653a
andrejsmikis <70051330+andrejsmikis@users.noreply.github.com>
2021-02-10 13:47:14 +0200

    pievienota cita tehnika html, js

--- js/pievieno_tehniku.js
+++ js/pievieno_tehniku.js
@@ -1,28 +1,64 @@
 async function pievieno_tehniku()
 {
 let tehnikasIzvele=document.querySelector('#tehnika').value;
-let requestBodyJson = {
-             // "skaits": parseInt(document.querySelector('#skaits').value),            
-             "tips": document.querySelector('#tehnika').value, 
-             "inventaraNr": document.querySelector('#inos2').value,
-             "nosaukums": document.querySelector('#inos3').value,
-             "iegadesGads": document.querySelector('#inos4').value,
-             "piegadatajs": document.querySelector('#inos5').value,
-             "razotajs" : document.querySelector('#inos6').value,
-             "kabinetaNr" : document.querySelector('#inos7').value,
-             "atbildigais" : document.querySelector('#inos8').value,
-             "procesors": document.querySelector('#inos9').value,
-             "ram": document.querySelector('#inos10').value,
-             "cietaisDisks": document.querySelector('#inos11').value,
-              switch (true){
-              case tehnikasIzvele == "dators"  :
-                "programmatura": document.querySelector('#inos12').value                 
-              break;
-              case tehnikasIzvele == "monitors"  :
-                "programmatura": document.querySelector('#inos13').value                 
-              break;
+switch (true){
+  case tehnikasIzvele == "dators"  :
+    let requestBodyJson = {
+      // "skaits": parseInt(document.querySelector('#skaits').value),            
+      "tips": document.querySelector('#tehnika').value, 
+      "inventaraNr": document.querySelector('#inos2').value,
+      "nosaukums": document.querySelector('#inos3').value,
+      "iegadesGads": document.querySelector('#inos4').value,
+      "piegadatajs": document.querySelector('#inos5').value,
+      "razotajs" : document.querySelector('#inos6').value,
+      "kabinetaNr" : document.querySelector('#inos7').value,
+      "atbildigais" : document.querySelector('#inos8').value,
+      "procesors": document.querySelector('#inos9').value,
+      "ram": document.querySelector('#inos10').value,
+      "cietaisDisks": document.querySelector('#inos11').value,              
+      "programmatura": document.querySelector('#inos12').value                 
+      }
+  break;
+  case tehnikasIzvele == "monitors"  :
+    let requestBodyJson = {
+      // "skaits": parseInt(document.querySelector('#skaits').value),            
+      "tips": document.querySelector('#tehnika').value, 
+      "inventaraNr": document.querySelector('#inos2').value,
+      "nosaukums": document.querySelector('#inos3').value,
+      "iegadesGads": document.querySelector('#inos4').value,
+      "piegadatajs": document.querySelector('#inos5').value,
+      "razotajs" : document.querySelector('#inos6').value,
+      "kabinetaNr" : document.querySelector('#inos7').value,
+      "atbildigais" : document.querySelector('#inos8').value,
+      "procesors": document.querySelector('#inos9').value,
+      "ram": document.querySelector('#inos10').value,
+      "cietaisDisks": document.querySelector('#inos11').value,              
+      "programmatura": document.querySelector('#inos13').value                 
+      }
+  break;
+  case tehnikasIzvele == "printeris"  :
+    let requestBodyJson = {
+      // "skaits": parseInt(document.querySelector('#skaits').value),            
+      "tips": document.querySelector('#tehnika').value, 
+      "inventaraNr": document.querySelector('#inos2').value,
+      "nosaukums": document.querySelector('#inos3').value,
+      "iegadesGads": document.querySelector('#inos4').value,
+      "piegadatajs": document.querySelector('#inos5').value,
+      "razotajs" : document.querySelector('#inos6').value,
+      "kabinetaNr" : document.querySelector('#inos7').value,
+      "atbildigais" : document.querySelector('#inos8').value,
+      "procesors": document.querySelector('#inos9').value,
+      "ram": document.querySelector('#inos10').value,
+      "cietaisDisks": document.querySelector('#inos11').value,              
+      "programmatura": document.querySelector('#inos14').value                 
+      }
+  break;
+} 
+                                 
+              
+            
              
-              };
+              
 let requestBodyString = JSON.stringify(requestBodyJson);
 
 
