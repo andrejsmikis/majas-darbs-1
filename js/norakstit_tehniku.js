9abe6a65695d3b2710cd1d03b215d619f5a3ea15
andrejsmikis <70051330+andrejsmikis@users.noreply.github.com>
2021-02-20 17:36:39 +0200

    tehnikas norakstīšana

---
+++ js/norakstit_tehniku.js
@@ -0,0 +1,21 @@
+async function norakstit_tehniku()
+{
+    let nid=document.querySelector('#inos1').value;
+//    let tehnikasIzvele=document.querySelector('#tehnika').value;
+console.log (nid);
+let request = await fetch('https://andrejstehnika.amikis.repl.co/api/'+nid+'/norakstit',
+    {
+    method:"POST",
+	headers:
+        {
+        //'X-API-KEY': 'abcd',
+        //'Content-Type': 'application/json'
+         }});
+
+//console.log(await request.json());
+
+
+console.log(await atbilde);
+//console.log (nid);
+
+}
