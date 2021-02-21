272c03bf160cfaddea784f8f40527292a0c9c291
andrejsmikis <70051330+andrejsmikis@users.noreply.github.com>
2021-02-21 11:06:16 +0200

    Update norakstit_tehniku.js

--- js/norakstit_tehniku.js
+++ js/norakstit_tehniku.js
@@ -2,11 +2,11 @@ async function norakstit_tehniku()
 {
     let nid=document.querySelector('#inos1').value;
 //    let tehnikasIzvele=document.querySelector('#tehnika').value;
-console.log (nid);
+
 let request = await fetch('https://andrejstehnika.amikis.repl.co/api/',
     {
     method:"POST",
-    body: {id:12},
+    body: {'id':nid}
 	headers: {
         //'X-API-KEY': 'abcd',
         'Content-Type': 'application/json'
