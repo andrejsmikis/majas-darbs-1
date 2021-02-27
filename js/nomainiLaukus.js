48beb8cff85463793e989349cdde1ba50fc77675
andrejsmikis <70051330+andrejsmikis@users.noreply.github.com>
2021-02-28 00:36:23 +0200

    dažādas tehnikas pievienošana

--- js/nomainiLaukus.js
+++ js/nomainiLaukus.js
@@ -3,34 +3,19 @@ function nomainiLaukus() {
     document.getElementById("datortehnika").innerHTML = x;
     switch  (x){
         case 'dators':
-        document.getElementById("bnos9").textContent = "procesors";
-        document.querySelector("#inos9").name="procesors";
-        document.getElementById("bnos10").textContent = "ram";
-        document.querySelector("#inos10").name="ram";
-        document.getElementById("bnos11").textContent = "hdd";
-        document.querySelector("#inos11").name="hdd";
-        document.getElementById("bnos12").textContent = "programmatūra";
-        document.querySelector("#inos12").name="programmatura";
+            document.querySelector("#dators").className = "radit";
+            document.querySelector("#printeris").className = "neradit"; 
+            document.querySelector("#monitors").className = "neradit";
         break;
         case 'monitors':
-        document.getElementById("bnos9").textContent = "tips";
-        document.querySelector("#inos9").name="mtips";
-        document.getElementById("bnos10").textContent = "izmērs";
-        document.querySelector("#inos10").name="izmers";
-        document.getElementById("bnos11").textContent = "video ieejas";
-        document.querySelector("#inos11").name="videoIeejas";
-        document.getElementById("bnos12").textContent = "skaņa";
-        document.querySelector("#inos12").name="skana";
+            document.querySelector("#dators").className = "neradit";
+            document.querySelector("#printeris").className = "radit";
+            document.querySelector("#monitors").className = "neradit";
         break;
         case 'printeris':
-        document.getElementById("bnos9").textContent = "tips";
-        document.querySelector("#inos9").name="ptips";
-        document.getElementById("bnos10").textContent = "pieslēgums";
-        document.querySelector("#inos10").name="pieslegums";
-        document.getElementById("bnos11").textContent = "papīra izmērs";
-        document.querySelector("#inos11").name="papiraIzmers";
-        document.getElementById("bnos12").textContent = "toneris";
-        document.querySelector("#inos12").name="toneris";
+            document.querySelector("#dators").className = "neradit";
+            document.querySelector("#printeris").className = "neradit";
+            document.querySelector("#monitors").className = "radit";
         //let tests=document.querySelector("#inos12").name;
         //document.getElementById("bnos13").textContent = tests;
         break;
